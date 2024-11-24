33e5df2d61c0fd1bd743fc1cec495cd10c30f323
hcyct0225 <yantong.he@student.mahidol.ac.th>
2024-11-24 04:44:25 +0000

    Added 10 test cases and fixed bugs in Calculator class

--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,25 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1, 2), 3)
 
     # Add the following test methods to the TestCalculator class:
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(1000, -500), 1500)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+        self.assertEqual(self.calc.multiply(100, -1), -100)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+        with self.assertRaises(ValueError):
+            self.calc.modulo(10, 0)
+
+    
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
